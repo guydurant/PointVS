ed6a64c6cce57a6c46f86e82480cb320128ca909
guidodee <guydurant@btinternet.com>
2022-06-08 11:30:59 +0100

    Added script to loop through inference

---
+++ point_vs/inference_mass_run.py
@@ -0,0 +1,38 @@
+import os
+
+casf_type_files = ['/data/localhost/not-backed-up/durant/casf_2016_pymol_redocked.types',
+'/data/localhost/not-backed-up/durant/casf_2016_pymol_crossdocked_most_similiar_structure.types',
+'/data/localhost/not-backed-up/durant/casf_2016_pymol_crossdocked_least_similiar_structure.types',
+'casf_2016_pymol.types']
+
+
+models = [
+'crystal_regression/PointBAP/crystal_regression/',
+'crystal_regression/PointBAP/crystal_regression_seq_100/',
+'crystal_regression/PointBAP/crystal_regression_seq_90/',
+'crystal_regression/PointBAP/crystal_regression_seq_30/',
+'crystal_regression/PointBAP/crystal_regression_tan_100/',
+'crystal_regression/PointBAP/crystal_regression_tan_90/',
+'crystal_regression/PointBAP/crystal_regression_tan_30/',
+'crystal_regression/PointBAP/crystal_regression_both_100/',
+'crystal_regression/PointBAP/crystal_regression_both_90/',
+'crystal_regression/PointBAP/crystal_regression_both_30/',
+'crystal_regression/PointBAP/crystal_regression_both_30_size_control/',
+'docked_regression/PointBAP/docked_regression/',
+'docked_regression/PointBAP/docked_regression_seq_100/',
+'docked_regression/PointBAP/docked_regression_seq_90/',
+'docked_regression/PointBAP/docked_regression_seq_30/',
+'docked_regression/PointBAP/docked_regression_tan_100/',
+'docked_regression/PointBAP/docked_regression_tan_90/',
+'docked_regression/PointBAP/docked_regression_tan_30/',
+'docked_regression/PointBAP/docked_regression_both_100/',
+'docked_regression/PointBAP/docked_regression_both_90/',
+'docked_regression/PointBAP/docked_regression_both_30/',
+'docked_regression/PointBAP/docked_regression_both_30_size_control/',
+
+]
+
+
+for i in casf_types_files:
+	for m in models:
+		os.system(f'python point_vs/inference.py ../{m}checkpoints/ {c} /data/localhost/not-backed-up/durant/pdbbind_2020_general_parquets  --wandb_project PointBAP --wandb_run inference')
